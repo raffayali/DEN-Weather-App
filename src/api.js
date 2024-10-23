67d7a5d1424bd9186cd06d30bf9d94547d44e721
raffayali <97637404+raffayali@users.noreply.github.com>
2024-10-23 21:02:17 +0500

    api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-const API_KEY = '2cf2007aeafa7a392f189b2b8ce7c7cf';
-const BASE_URL = 'https://api.openweathermap.org/data/2.5';
+const API_KEY = 'API_KEY';
+const BASE_URL = 'BASE_URL';
 
 export const fetchWeather = async (city) => {
   const response = await fetch(`${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`);
